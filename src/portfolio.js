ee81175d52c02e18e928a434beb6e98e8259bdd7
szczepulec <szczepan@letkiewicz.pl>
2026-02-20 15:10:04 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -121,6 +121,27 @@ https://fontawesome.com/icons?d=gallery */
   display: false // Set false to hide this section, defaults to true
 };
 
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+
 // Education Section
 
 const educationInfo = {
