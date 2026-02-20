61cd3e896a1403c4a5a70673889cbc8bfb69d86e
szczepulec <szczepan@letkiewicz.pl>
2026-02-20 14:45:51 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -209,7 +209,7 @@ const workExperiences = {
     {
       role: "Software Engineer Intern",
       company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
+      companylogo: require("./assets/images/quoraLogo.png"),
       date: "Jan 2015 – Sep 2015",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
