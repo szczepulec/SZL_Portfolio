c735be607da367e1519815c0b72288f2aa8e09d8
szczepulec <szczepan@letkiewicz.pl>
2026-07-31 11:29:15 +0200

    image test

--- src/portfolio.js
+++ src/portfolio.js
@@ -703,7 +703,7 @@ export const researchSection = {
       }
     },
     {
-      image: require("./assets/images/BigProjects/BSC_Thesis/BSC_Thesis_wind_Tunnel_1.jpg"),
+      image: require("./assets/images/BigProjects/BSC_Thesis/BSC_Thesis_cover.jpg"),
       projectName:
         "Bachelor Thesis",
       projectDesc:
